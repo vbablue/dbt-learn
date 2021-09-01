addbf29b49b433fa3474a64b0d2ad138f4f30b23
Sriram Vadlamani <sriram.vadlamani@gmail.com>
2021-09-01 18:19:58 +0000

    modified payment

--- models/staging/shop/stg_payment.sql
+++ models/staging/shop/stg_payment.sql
@@ -1,4 +1,8 @@
-select id as payment_id, orderid as order_id, paymentmethod as payment_method, status
+select 
+id as payment_id
+, orderid as order_id
+, paymentmethod as payment_method
+, status
 , amount / 100 as amount,
 created as created_at
 from stripe.payment
